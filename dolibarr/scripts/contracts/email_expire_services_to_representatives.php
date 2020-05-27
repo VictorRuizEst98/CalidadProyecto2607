ac823b55302e469653deec121e17460262f53c01
brayan-mendoza <atomic_daniel@live.com>
2020-05-27 17:36:18 -0500

    modificacion contratos2

--- dolibarr/scripts/contracts/email_expire_services_to_representatives.php
+++ dolibarr/scripts/contracts/email_expire_services_to_representatives.php
@@ -27,7 +27,8 @@
 $sapi_type = php_sapi_name();
 $script_file = basename(__FILE__);
 $path = __DIR__ . '/';
-
++define=("CONF",'confirm')
++define=("CONTRA",'contracts')
 // Test si mode batch
 $sapi_type = php_sapi_name();
 if (substr($sapi_type, 0, 3) == 'cgi') {
@@ -35,7 +36,7 @@ if (substr($sapi_type, 0, 3) == 'cgi') {
 	exit(- 1);
 }
 
-if (! isset($argv[1]) || ! $argv[1] || ! in_array($argv[1], array('test','confirm'))) {
+if (! isset($argv[1]) || ! $argv[1] || ! in_array($argv[1], array('test',CONF))) {
 	print "Usage: $script_file (test|confirm) [delay]\n";
 	print "\n";
 	print "Send an email to remind all contracts services to expire, to users that are sale representative for.\n";
@@ -48,7 +49,7 @@ $mode = $argv[1];
 require $path . "../../htdocs/master.inc.php";
 require_once DOL_DOCUMENT_ROOT . "/core/class/CMailFile.class.php";
 
-$langs->loadLangs(array('main','contracts'));
+$langs->loadLangs(array('main',CONTRA));
 
 // Global variables
 $version = DOL_VERSION;
@@ -67,20 +68,20 @@ $duration_value = isset($argv[2]) ? $argv[2] : 'none';
 
 print $script_file . " launched with mode " . $mode . " default lang=" . $langs->defaultlang . (is_numeric($duration_value) ? " delay=" . $duration_value : "") . "\n";
 
-if ($mode != 'confirm')
+if ($mode != CONF){
 	$conf->global->MAIN_DISABLE_ALL_MAILS = 1;
-
+}
 $sql = "SELECT DISTINCT c.ref, c.fk_soc, cd.date_fin_validite, cd.total_ttc, cd.description as description, p.label as plabel, s.rowid, s.nom as name, s.email, s.default_lang,";
 $sql .= " u.rowid as uid, u.lastname, u.firstname, u.email, u.lang";
 $sql .= " FROM " . MAIN_DB_PREFIX . "societe AS s, " . MAIN_DB_PREFIX . "contrat AS c, " . MAIN_DB_PREFIX . "contratdet AS cd";
 $sql .= " LEFT JOIN " . MAIN_DB_PREFIX . "product AS p ON p.rowid = cd.fk_product, " . MAIN_DB_PREFIX . "societe_commerciaux AS sc, " . MAIN_DB_PREFIX . "user AS u";
 $sql .= " WHERE s.rowid = c.fk_soc AND c.rowid = cd.fk_contrat AND c.statut > 0 AND cd.statut<5";
-if (is_numeric($duration_value))
-	$sql .= " AND cd.date_fin_validite < '" . $db->idate(dol_time_plus_duree($now, $duration_value, "d")) . "'";
+if (is_numeric($duration_value)){
+	$sql .= " AND cd.date_fin_validite < '" . $db->idate(dol_time_plus_duree($now, $duration_value, "d")) . "'";}
 $sql .= " AND sc.fk_soc = s.rowid AND sc.fk_user = u.rowid";
 $sql .= " ORDER BY u.email ASC, s.rowid ASC, c.ref ASC"; // Order by email to allow one message per email
 
-// print $sql;
+
 $resql = $db->query($sql);
 if ($resql) {
 	$num = $db->num_rows($resql);
@@ -103,8 +104,8 @@ if ($resql) {
 				if (dol_strlen($oldemail) && $oldemail != 'none') {
 					envoi_mail($mode, $oldemail, $message, $total, $oldlang, $oldsalerepresentative, $duration_value);
 				} else {
-					if ($oldemail != 'none')
-						print "- No email sent for " . $oldsalerepresentative . ", total: " . $total . "\n";
+					if ($oldemail != 'none'){
+						print "- No email sent for " . $oldsalerepresentative . ", total: " . $total . "\n";}
 				}
 				$oldemail = $obj->email;
 				$olduid = $obj->uid;
@@ -114,8 +115,8 @@ if ($resql) {
 				$total = 0;
 				$foundtoprocess = 0;
 				$salerepresentative = dolGetFirstLastname($obj->firstname, $obj->lastname);
-				if (empty($obj->email))
-					print "Warning: Sale representative " . $salerepresentative . " has no email. Notice disabled.\n";
+				if (empty($obj->email)){
+					print "Warning: Sale representative " . $salerepresentative . " has no email. Notice disabled.\n";}
 			}
 
 			// Define line content
@@ -123,7 +124,7 @@ if ($resql) {
 			$outputlangs->setDefaultLang(empty($obj->lang) ? $langs->defaultlang : $obj->lang); // By default language of sale representative
 
 			// Load translation files required by the page
-			$outputlangs->loadLangs(array("main","contracts","bills","products"));
+			$outputlangs->loadLangs(array("main",CONTRA,"bills","products"));
 
 			if (dol_strlen($obj->email)) {
 				$message .= $outputlangs->trans("Contract") . " " . $obj->ref . ": " . $langs->trans("Service") . " " . dol_concatdesc($obj->plabel, $obj->description) . " (" . price($obj->total_ttc, 0, $outputlangs, 0, 0, - 1, $conf->currency) . ") " . $obj->name . ", " . $outputlangs->trans("DateEndPlannedShort") . " " . dol_print_date($db->jdate($obj->date_fin_validite), 'day') . "\n\n";
@@ -131,10 +132,10 @@ if ($resql) {
 				$foundtoprocess ++;
 			}
 			print "Service to expire " . $obj->ref . ", label " . dol_concatdesc($obj->plabel, $obj->description) . ", due date " . dol_print_date($db->jdate($obj->date_fin_validite), 'day') . " (linked to company " . $obj->name . ", sale representative " . dolGetFirstLastname($obj->firstname, $obj->lastname) . ", email " . $obj->email . "): ";
-			if (dol_strlen($obj->email))
-				print "qualified.";
-			else
-				print "disqualified (no email).";
+			if (dol_strlen($obj->email)){
+				print "qualified.";}
+			else{
+				print "disqualified (no email).";}
 			print "\n";
 
 			unset($outputlangs);
@@ -181,21 +182,21 @@ function envoi_mail($mode, $oldemail, $message, $total, $userlang, $oldsalerepre
 {
 	global $conf, $langs;
 
-	if (getenv('DOL_FORCE_EMAIL_TO'))
-		$oldemail = getenv('DOL_FORCE_EMAIL_TO');
+	if (getenv('DOL_FORCE_EMAIL_TO')){
+		$oldemail = getenv('DOL_FORCE_EMAIL_TO');}
 
 	$newlangs = new Translate('', $conf);
 	$newlangs->setDefaultLang(empty($userlang) ? (empty($conf->global->MAIN_LANG_DEFAULT) ? 'auto' : $conf->global->MAIN_LANG_DEFAULT) : $userlang);
 	$newlangs->load("main");
-	$newlangs->load("contracts");
+	$newlangs->load(CONTRA);
 
 	if ($duration_value) {
-		if ($duration_value > 0)
-			$title = $newlangs->transnoentities("ListOfServicesToExpireWithDuration", $duration_value);
-		else
-			$title = $newlangs->transnoentities("ListOfServicesToExpireWithDurationNeg", $duration_value);
-	} else
-		$title = $newlangs->transnoentities("ListOfServicesToExpire");
+		if ($duration_value > 0){
+			$title = $newlangs->transnoentities("ListOfServicesToExpireWithDuration", $duration_value);}
+		else{
+			$title = $newlangs->transnoentities("ListOfServicesToExpireWithDurationNeg", $duration_value);}
+	} else{
+		$title = $newlangs->transnoentities("ListOfServicesToExpire");}
 
 	$subject = (empty($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_SUBJECT) ? $title : $conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_SUBJECT);
 	$sendto = $oldemail;
@@ -207,10 +208,10 @@ function envoi_mail($mode, $oldemail, $message, $total, $userlang, $oldsalerepre
 	dol_syslog("email_expire_services_to_representatives.php: send mail to " . $oldemail);
 
 	$usehtml = 0;
-	if (dol_textishtml($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_FOOTER))
-		$usehtml += 1;
-	if (dol_textishtml($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_HEADER))
-		$usehtml += 1;
+	if (dol_textishtml($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_FOOTER)){
+		$usehtml += 1;}
+	if (dol_textishtml($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_HEADER)){
+		$usehtml += 1;}
 
 	$allmessage = '';
 	if (! empty($conf->global->SCRIPT_EMAIL_EXPIRE_SERVICES_SALESREPRESENTATIVES_HEADER)) {
@@ -232,7 +233,7 @@ function envoi_mail($mode, $oldemail, $message, $total, $userlang, $oldsalerepre
 	$mail->errors_to = $errorsto;
 
 	// Send or not email
-	if ($mode == 'confirm') {
+	if ($mode == CONF) {
 		$result = $mail->sendfile();
 		if (! $result) {
 			print "Error sending email " . $mail->error . "\n";
